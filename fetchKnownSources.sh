ff5c6e7bac5836c427f2699abea8d406fadebd54
youngchief btw ツ <youngchief@youngchief.tk>
2021-04-01 18:06:57 +0000

    Initial Commit!

---
+++ fetchKnownSources.sh
@@ -0,0 +1,8 @@
+# rm -rf src/;
+mkdir -p src/pkg/;
+wget https://plus.minecraft.net/ -O src/index.html;
+wget https://plus.minecraft.net/box.jpg -O src/box.jpg;
+wget https://plus.minecraft.net/glowsquid.gif -O src/glowsquid.gif;
+wget https://plus.minecraft.net/pkg/mcse_web.js -O src/pkg/mcse_web.js;
+wget https://plus.minecraft.net/pkg/mcse_web_bg.wasm -O src/pkg/mcse_web_bg.wasm;
+wget https://plus.minecraft.net/screensaver.zip -O src/screensaver.zip;
