deceab961e56af4542b528573b5de15f50b61d98
youngchief btw ツ <youngchief@youngchief.tk>
2021-04-01 18:21:24 +0000

    Remove generated WASM binary!

--- fetchKnownSources.sh
+++ fetchKnownSources.sh
@@ -4,6 +4,5 @@ wget https://plus.minecraft.net/ -O src/index.html;
 wget https://plus.minecraft.net/box.jpg -O src/box.jpg;
 wget https://plus.minecraft.net/glowsquid.gif -O src/glowsquid.gif;
 wget https://plus.minecraft.net/pkg/mcse_web.js -O src/pkg/mcse_web.js;
-wget https://plus.minecraft.net/pkg/mcse_web_bg.wasm -O src/pkg/mcse_web_bg.wasm;
 wget https://plus.minecraft.net/screensaver.zip -O src/screensaver.zip;
 wget https://plus.minecraft.net/favicon.ico -O src/favicon.ico;
